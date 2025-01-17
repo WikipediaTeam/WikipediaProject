acc8da1f5ab5f911dfa29e46f9695883a5a2bf3e
Erik-Johansson13 <13ej1018@amal.se>
2025-01-17 14:51:27 +0100

    javascript

---
+++ homepagescript.js
@@ -0,0 +1,12 @@
+let buttons = document.getElementsByClassName("changeButtons")
+let texts = document.getElementsByClassName("text")
+let background = document.body
+
+
+function clicke(c){
+    if(c === 'white'){
+        background.style.backgroundColor = "#FFF"
+        texts.style.
+    }
+    else if(c === 'black')
+}
