11770fae30335150ff2e2d1f1cb8c53b5aa26b94
Erik-Johansson13 <13ej1018@amal.se>
2025-01-20 09:53:05 +0100

    infinf

--- Script.js
+++ Script.js
@@ -4,9 +4,9 @@ let head = document.head
 
 
 function clicke(c){
-    if(c === 'white'){
+    if(c === 'deepPurple'){
         head.innerHTML = "<meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><link rel='stylesheet' href='Style1.css'><script defer src='homepagescript.js' ></script><title>BadPedia</title>"
-    } else if(c === 'black'){
+    } else if(c === 'lightSimple'){
         head.innerHTML = "<meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><link rel='stylesheet' href='Style2.css'><script defer src='homepagescript.js' ></script><title>BadPedia</title>"
     }
 }
