c70d2a3f131f849991894a01b3c6da7a73a635ef
Erik-Johansson13 <13ej1018@amal.se>
2025-01-20 09:08:15 +0100

    new

---
+++ Script.js
@@ -0,0 +1,12 @@
+let buttons = document.getElementsByClassName("changeButtons")
+let texts = document.getElementsByClassName("text")
+let head = document.head
+
+
+function clicke(c){
+    if(c === 'white'){
+        head.innerHTML = "<meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><link rel='stylesheet' href='Style1.css'><script defer src='homepagescript.js' ></script><title>BadPedia</title>"
+    } else if(c === 'black'){
+        head.innerHTML = "<meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><link rel='stylesheet' href='Style2.css'><script defer src='homepagescript.js' ></script><title>BadPedia</title>"
+    }
+}
