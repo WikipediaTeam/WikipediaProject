8fb307d2763e060fe59f40129e1127015202f1c2
Erik-Johansson13 <13ej1018@amal.se>
2025-01-24 14:28:20 +0100

    nienf

--- Script.js
+++ Script.js
@@ -8,5 +8,8 @@ function clicke(c){
         head.innerHTML = "<meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><link rel='stylesheet' href='Style1.css'><script defer src='homepagescript.js' ></script><title>BadPedia</title>"
     } else if(c === 'lightSimple'){
         head.innerHTML = "<meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><link rel='stylesheet' href='Style2.css'><script defer src='homepagescript.js' ></script><title>BadPedia</title>"
+    } else if (c === 'gray'){
+        head.innerHTML = "<meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><link rel='stylesheet' href='Style3.css'><script defer src='homepagescript.js' ></script><title>BadPedia</title>"
     }
+    
 }
